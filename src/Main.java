82d0a3845de7a52cee5e60ef9ffcb21462d94fe0
Andrey P <AndreyP@home>
2023-09-26 20:43:08 +0300

    777

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,24 @@ public class Main {
         int floor = kb.nextInt();
         Elevator(floor);*/
 
+
         System.out.print("Введите число: ");
-        double fact = kb.nextDouble();
+        double fact = kb.nextInt();
         System.out.println("%.0f! = %.0f".formatted(fact, Fractorial(fact)));
         //Fractorial(fact);
 
 
-        System.out.print("Введите целое число и степень: ");
+        /*System.out.print("Введите целое число и степень: ");
         double number = kb.nextInt();
-        double degre = kb.nextInt();
-        System.out.println("%.0f^%.0f = %.0f".formatted(number,degre,Power(number,degre)));
-
-        System.out.print("Введите число предела: ");
+        int degre = kb.nextInt();
+        System.out.println("%.0f^%d = %.0f".formatted(number, degre, Power(number, degre)));
+*/
+        System.out.print("Введите предельное число: ");
         int fib = kb.nextInt();
-        System.out.println(Fibonacci(fib));
+        //System.out.println(Fibonacci(fib));
+        Fibonacc(fib, 0, 1);
     }
+
 
     static void Elevator(int floor) {
         if (floor == 0) {
@@ -32,29 +35,38 @@ public class Main {
             return;
         }
         System.out.println("Вы на " + floor + " этаже");
-        Elevator(floor-1);
+        Elevator(floor - 1);
         System.out.println("Вы на " + floor + " этаже");
 
     }
 
-    static double Fractorial(double fact){
+    static double Fractorial(double fact) {
+        /*if(fact < 0) return 0;
         if (fact == 1)
         return 1;
         return fact * Fractorial(fact - 1);
+        */
+        return fact < 0 ? 0 : fact == 0 ? 1 : fact * Fractorial(fact - 1);
 
     }
 
-    static double Power(double number, double degre){
-        if(degre == 1) return number;
-       return number * Power(number,degre - 1 );
+    //static double Power(double number, int degre) {
+        /*if(degre == 0) return 1;
+       return number * Power(number,degre - 1 );*/
 
-    }
-    static int Fibonacci(int fib){
-        if(fib == 0 || fib == 1) return fib;
-        return Fibonacci(fib - 1) + Fibonacci(fib - 2);
+        //return degre == 0 ? 1 : degre > 0 ? number * Power( number,degre - 1 ) : 1/ number * Power( number,degre +1 );
 
+
+        //static int Fibonacci(int fib){
+       /* if(fib == 0 || fib == 1) return fib;
+        return Fibonacci(fib - 1) + Fibonacci(fib - 2);*/
+
+        static void Fibonacc(long fib, long a, long b ){
+            if (a > fib) return;
+            System.out.print(a + "\t");
+            Fibonacc(fib, b, a + b);
+        }
     }
-}
 
 //stack
 //stack overflow exception
